d3bed1f148b9a07cb64b5419e42f124fbdea712a
himynameisfil <nguye208@gmail.com>
2017-04-12 22:11:06 -0400

    Initial Load

---
+++ GradleTest/src/main/com/PhilFolder/mainDriver.java
@@ -0,0 +1,13 @@
+package com.PhilFolder;
+
+public class mainDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World from that java jar!");
+	}
+
+}
